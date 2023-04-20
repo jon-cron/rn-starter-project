8064c6c23014d5af727d43a87814927d2dbbd676
jon-cron <jonathanacm95@gmail.com>
2023-04-20 10:10:12 -0600

    styling

--- src/screens/StartScreen.jsx
+++ src/screens/StartScreen.jsx
@@ -5,15 +5,19 @@ import PrimaryButton from "../components/PrimaryButton.js";
 
 const StartScreen = () => {
   return (
-    <>
+    <View style={styles.container}>
       <Input placeholder="number" label="Number" />
       {/* NOTE we are wrapping our text in the comp and using children to render different text */}
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default StartScreen;
