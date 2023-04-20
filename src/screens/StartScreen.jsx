9e6228bdc27d7e2421221d33b55c7f5a47328dab
jon-cron <jonathanacm95@gmail.com>
2023-04-20 11:04:21 -0600

    styling

--- src/screens/StartScreen.jsx
+++ src/screens/StartScreen.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Text, View, StyleSheet, Button, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 import { Input } from "react-native-elements";
 import PrimaryButton from "../components/PrimaryButton.js";
 
 const StartScreen = () => {
   return (
     <View style={styles.container}>
-      <Input placeholder="number" label="Number" />
+      <TextInput style={styles.numberInput} />
       {/* NOTE we are wrapping our text in the comp and using children to render different text */}
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -24,12 +31,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#72063c",
     borderRadius: 8,
     // NOTE elevation is android specific and will not effect apple apps
-    elevation: 5,
+    elevation: 10,
     // NOTE using these 4 shadow features together will control the shadow on apple devices. I was unable to see any shadow until using all 4
     shadowColor: "black",
     shadowOffset: { width: 0, height: 10 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    // NOTE unable to border until setting the width
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
   },
 });
 
