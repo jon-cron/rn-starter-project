6d417442407d29ffe9a1c2f27e574e3196d6228e
jon-cron <jonathanacm95@gmail.com>
2023-04-20 12:48:19 -0600

    push

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -6,21 +6,35 @@ const PrimaryButton = ({ children }) => {
     console.log("pressed", children);
   };
   return (
-    <Pressable onPress={handlePress}>
-      <View style={styles.container}>
-        <Text>{children}</Text>
-      </View>
-    </Pressable>
+    <View style={styles.buttonInnerContainer}>
+      <Pressable onPress={handlePress} android_ripple={{ color: "#640233" }}>
+        <Text style={styles.btnText}>{children}</Text>
+      </Pressable>
+    </View>
+    // NOTE this will look funny when clicked on Android devices. You should instead wrap the pressable in the view for better UI
+    // <Pressable onPress={handlePress} android_ripple={{ color: "#640233" }}>
+    //   <View style={styles.container}>
+    //     <Text style={styles.btnText}>{children}</Text>
+    //   </View>
+    // </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#72063c",
+  buttonOuterContainer: {
     borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 5,
+  },
+  btnText: {
+    color: "white",
+    textAlign: "center",
   },
 });
 
