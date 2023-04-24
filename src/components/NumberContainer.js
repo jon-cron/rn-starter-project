b291c6a025c0fb01f5215e123410038a37807567
jon-cron <jonathanacm95@gmail.com>
2023-04-24 16:07:27 -0600

    push

--- src/components/NumberContainer.js
+++ src/components/NumberContainer.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { Text, View, StyleSheet, Button, Pressable } from "react-native";
+import Colors from "../utilities/colors.js";
 
 const NumberContainer = ({ children }) => {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>{children}</Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderWidth: 3,
+    borderColor: Colors.secondary500,
+    padding: 24,
+    borderRadius: 8,
+    margin: 24,
+  },
+  content: {
+    textAlign: "center",
+    color: Colors.secondary500,
+    fontWeight: "bold",
+    fontSize: 36,
+  },
+});
 
 export default NumberContainer;
