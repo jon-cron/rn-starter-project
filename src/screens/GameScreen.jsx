ffb524230e17fbcc33681403879750519d857d1e
jon-cron <jonathanacm95@gmail.com>
2023-04-24 13:46:30 -0600

    game screen style

--- src/screens/GameScreen.jsx
+++ src/screens/GameScreen.jsx
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet, Button } from "react-native";
 const GameScreen = () => {
   return (
     <View style={styles.screen}>
-      <Text>Opponent's Guess</Text>
+      <Text style={styles.title}>Opponent's Guess</Text>
       <View>
         <Text>Higher or Lower</Text>
       </View>
@@ -18,6 +18,12 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
     marginTop: 30,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#ddb52f",
+    textAlign: "center",
   },
 });
 
